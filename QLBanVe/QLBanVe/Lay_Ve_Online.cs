a560d4a9356b62fd416c062acd111c291ecc6326
ThuyQuyen <1354052177quyen@ou.edu.vn>
2016-12-27 17:59:41 +0700

    Add code form Lay_ve_online

--- QLBanVe/QLBanVe/Lay_Ve_Online.cs
+++ QLBanVe/QLBanVe/Lay_Ve_Online.cs
@@ -13,16 +13,22 @@ namespace QLBanVe
 {
     public partial class Lay_Ve_Online : Form
     {
-
+        public delegate void SendMessage(string Message);
+        public SendMessage Sender;
+        string cnStr = "";
+        DataSet ds;
+        SqlConnection cn;
         public Lay_Ve_Online()
         {
             InitializeComponent();
             //Tạo con trỏ tới các hàm
-            
+            Sender = new SendMessage(DienThoai);
         }
-
         //Hàm có nhiệm vụ lấy tham số truyền vào
-        
+        public void DienThoai(string Message)
+        {
+            lbDienThoai.Text = Message;
+        }
 
         private void btQuayLai_Click(object sender, EventArgs e)
         {
@@ -37,7 +43,14 @@ namespace QLBanVe
 
         private void btLayVe_Click(object sender, EventArgs e)
         {
-
+            Thong_Tin_Ve frm = new Thong_Tin_Ve();
+            frm.Sender(lbPhim.Text);
+            frm.Send(lbSoVe.Text);
+            frm.send(lbHangGhe.Text);
+            frm.mess(lbSuatChieu.Text);
+            frm.message(lbRapChieu.Text);
+            this.Hide();
+            frm.Show();
         }
 
         private void btHuy_Click(object sender, EventArgs e)
@@ -58,7 +71,40 @@ namespace QLBanVe
 
         private void Lay_Ve_Online_Load(object sender, EventArgs e)
         {
+            cnStr = ConfigurationManager.ConnectionStrings["cnStr"].ConnectionString;
+            cn = new SqlConnection(cnStr);
+            string sql = "SELECT KH.HoKH, KH.TenKH, DK.TenPhim, DK.ViTri, DK.RapChieu, DK.SoLuong, DK.SuatChieu, DK.Gia FROM KhachHang KH, DangKyOnline DK WHERE KH.DienThoai = DK.DienThoai AND DK.DienThoai = " + lbDienThoai.Text;
+            dgvLayVe.DataSource = GetHoaDon(sql).Tables[0];
+            lbHo.Text = dgvLayVe.Rows[0].Cells[0].Value.ToString();
+            lbTen.Text = dgvLayVe.Rows[0].Cells[1].Value.ToString();
+            lbPhim.Text = dgvLayVe.Rows[0].Cells[2].Value.ToString();
+            lbSoVe.Text = dgvLayVe.Rows[0].Cells[3].Value.ToString();
+            lbSuatChieu.Text = dgvLayVe.Rows[0].Cells[4].Value.ToString();
+            lbHangGhe.Text = dgvLayVe.Rows[0].Cells[5].Value.ToString(); ;
+            lbRapChieu.Text = dgvLayVe.Rows[0].Cells[6].Value.ToString();
+            dgvLayVe.Hide();
+        }
+        public DataSet GetHoaDon(string sql)
+        {
+            try
+            {
+                SqlDataAdapter da = new SqlDataAdapter(sql, cn);
+                ds = new DataSet();
+                da.Fill(ds);
+                return ds;
+            }
+            catch (SqlException ex)
+            {
+                MessageBox.Show(ex.Message);
+                return null;
+            }
+            finally
+            {
+                cn.Close();
+            }
 
-        } 
+        }
+
+        
     }
 }
